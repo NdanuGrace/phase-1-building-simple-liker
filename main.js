1fe394c5e100708cd0fcaf8c80eae4aed8dcc37a
gracekyalo <gracendanu27@gmail.com>
2022-08-30 00:10:33 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const likeColor= "red"
 
 // Your JavaScript code goes here!
+
+const articleHearts = document.querySelectorAll(".like-glyph");
+function likeHeart(e){
+  const heart = e.target;
+  
+  mimicServerCall()
+  .then(function(serverMessage){
+    alert("server notified");
+    alert(serverMessage);
+    heart.innerText= EMPTY_HEART
+    heart.innerText= FULL_HEART
+    heart.style.color= likeColor
+
+  })
+  .catch(function(){
+    setTimeout(function(){
+      const errorMessage= document.querySelector('.hidden')
+      if(isNaN(document.querySelectorAll(".like-glyph").value)){
+        
+
+      }
+    }, 300);
+   
+    
+  });
+ }
+
+for(const glyph of articleHearts){
+  glyph.addEventListener('click', likeHeart)
+}
+
 
 
 
